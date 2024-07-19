3e415d756729dabe6548237ce83d8dead53806c4
Jerry Kingsbury <jerry.daniel.kingsbury@gmail.com>
2024-07-19 14:00:17 -0500

    Updating config
    Added some of the old configurations I had in cmp back.

--- .config/nvim/Lua/config/plugins/cmp.lua
+++ .config/nvim/Lua/config/plugins/cmp.lua
@@ -7,12 +7,15 @@ return {
 		dependencies = {
 			"saadparwaiz1/cmp_luasnip",
 			"rafamadriz/friendly-snippets",
+			"kristijanhusak/vim-dadbod-completion",
+			"onsails/lspkind.nvim",
 		},
 	},
 	{
 		"hrsh7th/nvim-cmp",
 		config = function()
 			local cmp = require("cmp")
+			local lspkind = require("lspkind")
 			require("luasnip.loaders.from_vscode").lazy_load()
 
 			cmp.setup({
@@ -22,7 +25,7 @@ return {
 					end,
 				},
 				mapping = cmp.mapping.preset.insert({
-          ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+					["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
 					["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
@@ -33,9 +36,18 @@ return {
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
 					{ name = "luasnip" }, -- For luasnip users.
+					{ name = "copilot" },
 				}, {
 					{ name = "buffer" },
 				}),
+				formatting = {
+					format = lspkind.cmp_format({
+						maxwidth = 50,
+						ellipsis_char = "...",
+						show_labelDetails = true,
+						symbol_map = { Copilot = "" },
+					}),
+				},
 			})
 			-- setup vim-dadbod
 			cmp.setup.filetype({ "sql" }, {
