1975027beff3aba20361e41a778d712fef04f83c
jdkingsbury <33006469+jdkingsbury@users.noreply.github.com>
2024-11-18 17:47:00 -0600

    Delete nvim(lazyvim) directory

--- nvim(lazyvim)/lua/plugins/colorscheme.lua
+++
@@ -1,8 +0,0 @@
-return {
-  {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    lazy = true,
-    priority = 1000,
-  },
-}
--- nvim(lazyvim)/lua/plugins/utils.lua
+++
@@ -1,6 +0,0 @@
-return {
-	{ "preservim/vim-pencil" },
-	{ "tpope/vim-dotenv" },
-	{ "mbbill/undotree" },
-  { "christoomey/vim-tmux-navigator" }, -- tmux & split window navigation
-}
